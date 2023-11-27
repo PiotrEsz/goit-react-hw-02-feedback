ff641c76791b38a43d0879aabc8909cad67ac305
Piotr Szatyński <szatynskip@gmail.com>
2023-11-27 12:50:25 +0100

    updates

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,40 @@
+import Statistics from './Statistics.jsx';
+import FeedbackOptions from './FeedbackOptions.jsx';
+
+import React, { useState } from 'react';
+
 export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  function onClickButtons(e) {
+    if (e === 'Good') {
+      setGood(e => e + 1);
+    }
+    if (e === 'Neutral') {
+      setNeutral(e => e + 1);
+    }
+    if (e === 'Bad') {
+      setBad(e => e + 1);
+    }
+  }
+
+  const total = good + bad + neutral;
+  const percent = Math.floor((good / total) * 100);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className="container">
+      <div className="wrapper">
+        <FeedbackOptions onClickButtons={onClickButtons} />
+        <Statistics
+          good={good}
+          bad={bad}
+          neutral={neutral}
+          total={total}
+          percent={percent}
+        />
+      </div>
     </div>
   );
 };
